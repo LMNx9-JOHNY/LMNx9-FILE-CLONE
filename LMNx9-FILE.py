c2a9aac8e207d85a790d2f9882f9e69b354686f4
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2023-11-27 23:44:54 +0600

    Create LMNx9-FILE.py

---
+++ LMNx9-FILE.py
@@ -0,0 +1,10 @@
+#__________________________________________________#
+#___________DARK_TEAM_LMNx9_______
+#______Kire Banchud File Nite Aisos...🧐😈
+#______Tore Open Challenge re hala bokchud..
+#______Parle Decode Kore Dekha....😹🤣
+#______Decode Korte Na Parle Tui Akta Madarcud...
+#__________________________________________________#
+import os
+import AFL
+AFL.loging()
